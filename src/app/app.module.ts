7aa7b65666891c9f38b6d8822a7fda25363e6894
brev <armistead.michael@gmail.com>
2017-10-23 09:37:03 -0500

    bootstrap and ngx-bootstrap added and working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from "@angular/platform-browser";
+import {NgModule} from "@angular/core";
 
-import { AppComponent } from './app.component';
+import {AppComponent} from "./app.component";
+import {AlertModule} from "ngx-bootstrap";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    AlertModule.forRoot(),
     BrowserModule
   ],
   providers: [],

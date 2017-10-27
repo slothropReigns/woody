5389c643d210bf26e4903250656cc87bbdd592aa
brev <armistead.michael@gmail.com>
2017-10-27 13:49:10 -0500

    fixed some broken links and input fields in nav areas / AIO page

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { AuthService } from '../auth/auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: [ './header.component.css' ]
 })
 export class HeaderComponent implements OnInit {
+  isLoggedIn = new BehaviorSubject<boolean>(false);
 
-  constructor() { }
+  constructor(private authService: AuthService) {
+    this.authService.isLoggedIn().subscribe(this.isLoggedIn);
+  }
 
   ngOnInit() {
+  }
+
+  currentUser() {
+    return this.authService.currentUser();
   }
 
 }
